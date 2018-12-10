6b4539c7ac1f8d479a9731f44c57de5e646d3778
Alejandro <alejandrochang10@gmail.com>
2018-12-09 21:29:19 -0800

    google api

---
+++ src/apis/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const KEY ='AIzaSyDt_zFb5ae1hxYViOP7Ipny0EgaTx33Osc';
+
+export default axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3/",
+  params: {
+   part: 'snippet',
+   maxResults: 5,
+   key: KEY
+  }
+});
