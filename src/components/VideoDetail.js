2929af3c5176ff3bc29f0cdb84e57d74a91b6400
Alejandro <alejandrochang10@gmail.com>
2018-12-12 21:54:50 -0800

    video rendering

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -5,13 +5,18 @@ const VideoDetail = ({ video }) => {
     return <div>Loading...</div>;
   }
 
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+
   return (
     <div>
+      <div className="ui embed">
+        <iframe title="video player" src={videoSrc}/>
+      </div>
       <div className="ui segment">
-        <div className="ui header">
+        <h4 className="ui header">
           {video.snippet.title}
-        </div>
-        <p></p>
+          <p>{video.snippet.description}</p>
+        </h4>
       </div>
     </div>
   );
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,10 +1,14 @@
 import React from "react";
-import './VideoItem.css';
+import "./VideoItem.css";
 
-const VideoItem = ({ video , onVideoSelect }) => {
+const VideoItem = ({ video, onVideoSelect }) => {
   return (
     <div onClick={() => onVideoSelect(video)} className="video-item item">
-      <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+      <img
+        alt={video.snippet.title}
+        className="ui image"
+        src={video.snippet.thumbnails.medium.url}
+      />
       <div className="content">
         <div className="header">Title: {video.snippet.title}</div>
       </div>
