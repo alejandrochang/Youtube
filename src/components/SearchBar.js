0653ba3405dea7411c708dd0ae80a87dc091fa19
Alejandro <alejandrochang10@gmail.com>
2018-12-12 22:27:05 -0800

    changing search results on youtube api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,22 +3,30 @@ import React from "react";
 class SearchBar extends React.Component {
   state = { term: "" };
 
-  onFormSubmit = (event) => {
+  onFormSubmit = event => {
     event.preventDefault();
-    
+
     this.props.onTermSubmit(this.state.term);
   };
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form  onSubmit={this.onFormSubmit} className="ui form">
+        <form onSubmit={this.onFormSubmit} className="ui form">
           <div className="field">
             <label>Video Search:</label>
-            <img style={{ position: "absolute", right: "1%", width: "30px", top: "42%"}} src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-512.png"/>
+            <img
+              style={{
+                position: "absolute",
+                right: "1%",
+                width: "30px",
+                top: "42%"
+              }}
+              src="https://cdn1.iconfinder.com/data/icons/hawcons/32/698627-icon-111-search-512.png"
+            />
             <input
               value={this.state.term}
-              onChange={(event) => this.setState({ term: event.target.value }) }
+              onChange={event => this.setState({ term: event.target.value })}
               placeholder="Search"
               type="text"
             />
